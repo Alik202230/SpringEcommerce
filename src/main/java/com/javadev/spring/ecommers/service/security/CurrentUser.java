d5e0d727162656c49ded33fe2ee6db69659ef0bb
Alik Sargsyan <alik.sargsyan@coderepablic.am>
2025-01-07 02:16:58 +0400

    Initial commit

--- src/main/java/com/javadev/spring/ecommers/service/security/CurrentUser.java
+++ src/main/java/com/javadev/spring/ecommers/service/security/CurrentUser.java
@@ -6,7 +6,7 @@ import com.javadev.spring.ecommers.model.User;
 
 public class CurrentUser extends org.springframework.security.core.userdetails.User{
 
-  private User user;
+  private transient User user;
 
   public CurrentUser(User user) {
     super(user.getEmail(), user.getPassword(), AuthorityUtils.createAuthorityList(user.getRole().name())); 
@@ -15,6 +15,31 @@ public class CurrentUser extends org.springframework.security.core.userdetails.U
 
   public User getUser() {
     return user;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = super.hashCode();
+    result = prime * result + ((user == null) ? 0 : user.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (!super.equals(obj))
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    CurrentUser other = (CurrentUser) obj;
+    if (user == null) {
+      if (other.user != null)
+        return false;
+    } else if (!user.equals(other.user))
+      return false;
+    return true;
   }
 
 }
